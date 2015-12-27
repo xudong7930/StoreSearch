b7047eb26db4e62095efa8f276b086b4368f3859
xudong7930 <xudong7930@gmail.com>
2015-12-27 14:19:15 +0800

    renamed ViewController file to SearchViewController.

---
+++ StoreSearch20151227/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StoreSearch20151227
+//
+//  Created by xudong7930 on 12/27/15.
+//  Copyright © 2015 xudong7930. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    
+
+
+}
+
